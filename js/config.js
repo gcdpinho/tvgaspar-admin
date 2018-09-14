ee74a2d21860e6df08cfb02702f2e944ec9cfe56
gcdpinho <gcdpinho@gmail.com>
2018-09-14 00:20:23 -0300

    update url prod

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
-var serverUrl = "http://ec2-54-94-202-13.sa-east-1.compute.amazonaws.com:3000/";
+// var serverUrl = "http://ec2-54-94-202-13.sa-east-1.compute.amazonaws.com:3000/";
+var serverUrl = "http://http://93.188.167.194:3000/";
 // var serverUrl = "https://tvgaspar-api.herokuapp.com/";
 // var serverUrl = "http://localhost:3000/";
